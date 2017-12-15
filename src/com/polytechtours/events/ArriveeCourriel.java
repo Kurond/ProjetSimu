d55933f60463132b7139343d8e1f47053df08b63
Mikael1006 <mikael.moreau1006@gmail.com>
2017-12-15 17:44:51 +0100

    init mika

---
+++ src/com/polytechtours/events/ArriveeCourriel.java
@@ -0,0 +1,14 @@
+package com.polytechtours.events;
+
+import com.polytechtours.systeme.Variables;
+
+/**
+ * Created by mika on 15/12/17.
+ */
+public class ArriveeCourriel extends Event{
+
+    @Override
+    public void execute(Variables variables) {
+
+    }
+}
