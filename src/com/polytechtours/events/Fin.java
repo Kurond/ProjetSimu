77eefac45adb6c8bcb8376b596582fcd07953909
antoine <antoine.bernier59@gmail.com>
2017-12-18 16:39:33 +0100

    add event fin

--- src/com/polytechtours/events/Fin.java
+++ src/com/polytechtours/events/Fin.java
@@ -3,7 +3,7 @@ package com.polytechtours.events;
 import com.polytechtours.systeme.Variables;
 
 public class Fin extends Event {
-²
+
     @Override
     public void execute(Variables variables) {
 
